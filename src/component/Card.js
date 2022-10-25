482326844af5e56a8beac3793e65d510c485885b
anadda <63113056+anadda@users.noreply.github.com>
2022-10-25 13:41:10 +0700

    penambahan props

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import './Card.css';
+
 function Card(props) {
   useEffect(() => {
     alert('Kartu Nama Ditampilkan');
@@ -9,12 +10,21 @@ function Card(props) {
   }, []);
   return (
     <div className="card">
-      <img src="avatar.png" alt="Avatar" />
+      {/* <img src="avatar.png" alt="Avatar" /> */}
       <div className="container">
-        <h4>{props.nama}</h4>
-        <p>Kelompok {props.kelompok}</p>
+        {/* <h4>{props.nama}</h4> */}
+        <p>Nama Lengkap: {props.nama_lengkap}</p>
+        <p>Nama Panggilan: {props.nama_panggilan}</p>
+        <p>Nomor Telepon: {props.nomor_telepon}</p>
+        <p>Id Line: {props.id_line}</p>
+        <p>Tanggal Lahir: {props.tanggal_lahir}</p>
+        <p>NIM: {props.nim}</p>
+        <p>Email: {props.email}</p>
+        <p>Hobi: {props.hobi}</p>
+        {/* <p style={{ whiteSpace: "pre" }}>blablaba</p> */}
       </div>
     </div>
   );
 }
+
 export default Card;

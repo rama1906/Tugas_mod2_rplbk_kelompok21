e8aaf6f33b54219978578344ff58e3eed220198e
d4em0n <n0psledbyte@gmail.com>
2022-10-25 13:46:54 +0700

    fetch data & prepare finddata

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,11 +1,46 @@
 import React, { useEffect } from 'react';
 import './Form.css';
+
+const finddata = (mhs_data, data_search) => {
+  let res;
+  var mhs;
+  res = mhs_data.filter((mhs) => {
+    const lower = Object.values(mhs).map((val) => val.toString().toLowerCase());
+    return (
+      lower.findIndex((val) => val.includes(data_search.toLowerCase())) !== -1
+    );
+  });
+  if (res.length === 0) {
+    console.log("Data tidak ditemukan");
+  } else if (res.length > 1) {
+    console.log("Beberapa data ditemukan, mohon lebih spesifik");
+  } else {
+    mhs = res[0];
+  }
+  console.log(res);
+  return res;
+};
+
 function Form(props) {
   const [nama, setNama] = React.useState('');
   const [kelompok, setKelompok] = React.useState('');
   const [namaIsValid, setNamaIsValid] = React.useState(null);
   const [kelompokIsValid, setKelompokIsValid] = React.useState(null);
   const [formIsValid, setFormIsValid] = React.useState(false);
+  const data_url =
+    "https://gist.githubusercontent.com/d4em0n/aa26675e79cacd3dfcfab1552e3ae37e/raw/997b218a855f76aad5e319aaee65f636d235b1e9/data_aegis.txt";
+  useEffect(() => {
+    console.log("Dipanggil nih bos");
+    fetch(data_url)
+      .then((result) => {
+        return result.text();
+      })
+      .then((text) => {
+        console.log(text);
+        setDataMhs(eval(text));
+      });
+  }, []);
+  
   const submitHandler = (event) => {
     event.preventDefault();
     if (formIsValid) {
